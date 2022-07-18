4e7d3f4103d1af1c42aa7b11cbe655d7c93ab587
Hamza Cherkaoui WX1143141 <hamza.cherkaoui@huawei.com>
2022-07-18 18:13:42 +0200

    FIX rename controller.py file

--- bandpy/controller.py
+++ bandpy/controller.py
@@ -11,17 +11,33 @@ class DecentralizedController(Controller):
 
     def __init__(self, N, agent_cls, agent_kwargs):
         """Init."""
+        self.done = False
         super().__init__(N=N, agent_cls=agent_cls, agent_kwargs=agent_kwargs)
+
+    @property
+    def best_arms(self):
+        """Return for each agent the estimated best arm. """
+        best_arms = dict()
+        for agent_name, agent in self.agents.items():
+            best_arms[agent_name] = agent.best_arm
+        return best_arms
 
     def act(self, observations, rewards):
         """Make each agent choose an arm in a decentralized way."""
-        actions = dict()
+        actions, dones = dict(), []
         for agent_name in observations.keys():
 
             observation = observations[agent_name]
             reward = rewards[agent_name]
 
-            k = self.agents[agent_name].act(observation, reward)
+            agent = self.agents[agent_name]
+
+            k = agent.act(observation, reward)
             actions[agent_name] = k
+
+            if hasattr(agent, 'done'):
+                dones.append(agent.done)
+
+        self.done = (len(dones) != 0) & all(dones)
 
         return actions
