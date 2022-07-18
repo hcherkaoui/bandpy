5391b03d9c67cd540bb4a2b57c51db44adc26fb0
Hamza Cherkaoui WX1143141 <hamza.cherkaoui@huawei.com>
2022-07-18 18:05:04 +0200

    ENH better plotting opt and rendering

--- bandpy/plotting.py
+++ bandpy/plotting.py
@@ -1,17 +1,16 @@
 """ Define plotting utility functions in Bandpy. """
 # Authors: Hamza Cherkaoui <hamza.cherkaoui@huawei.com>
 
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_regrets(all_regrets, fig_dirname, verbose=False):
+def plot_regrets(all_regrets, fig_fname=None, verbose=False):
     """ Plot the regrets for all the environments and controllers."""
 
     env_names = list(all_regrets.keys())
 
-    fig, axis = plt.subplots(nrows=len(env_names), ncols=1, sharex=True,
+    fig, axis = plt.subplots(nrows=len(env_names), ncols=1,
                              figsize=(9, 6 * len(env_names)))
 
     if len(env_names) == 1:
@@ -29,20 +28,24 @@ def plot_regrets(all_regrets, fig_dirname, verbose=False):
             T = len(mean_regrets)
 
             axis[axis_idx].plot(np.arange(T), mean_regrets, lw=2.0,
-                                label=controller_name)
+                                label=f"R_t ({controller_name})")
             axis[axis_idx].fill_between(np.arange(T),
                                         mean_regrets + std_regrets / 2.0,
                                         mean_regrets - std_regrets / 2.0,
                                         alpha=0.3)
 
-        axis[axis_idx].legend(ncol=2, loc='upper center', fontsize=12)
-        axis[axis_idx].set_xlabel('t', fontsize=18)
-        axis[axis_idx].set_ylabel('R_t', fontsize=18, rotation=0)
-        axis[axis_idx].set_title(f"Environment: {env_name}", fontsize=18)
+        axis[axis_idx].axhline(lw=1.5, color='black')
+        axis[axis_idx].legend(ncol=2, loc='upper center', fontsize=13)
+        axis[axis_idx].set_xlabel('t', fontsize=16)
+        axis[axis_idx].set_title(f"Env: {env_name}", fontsize=16)
         axis[axis_idx].grid()
 
-    fig_fname = os.path.join(fig_dirname, 'regrets_evolution.png')
-    fig.savefig(fig_fname, dpi=300)
+    fig.tight_layout()
 
-    if verbose:
-        print(f"[Main] Saving '{fig_fname}'.")
+    if fig_fname is not None:
+        fig.savefig(fig_fname, dpi=300)
+
+        if verbose:
+            print(f"[Main] Saving '{fig_fname}'.")
+    else:
+        plt.plot()
