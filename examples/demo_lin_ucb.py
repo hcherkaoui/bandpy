54885dc0adc0b6537d86b0df0dc00f1d94421c3f
Hamza Cherkaoui WX1143141 <hamza.cherkaoui@huawei.com>
2022-11-25 11:24:10 +0100

    EX slight update example

--- examples/demo_lin_ucb.py
+++ examples/demo_lin_ucb.py
@@ -10,7 +10,7 @@ import time
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-from bandpy import run_trials, env, controller, agents, utils
+from bandpy import run_trials, env, controller, multi_agents, utils
 
 import matplotlib as mpl
 mpl.rcParams['text.usetex'] = True
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     t0_total = time.time()
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--n-trials', type=int, default=5,
+    parser.add_argument('--n-trials', type=int, default=1,
                         help='Number of trials.')
     parser.add_argument('--N', type=int, default=100,
                         help='Number of agents.')
@@ -43,6 +43,9 @@ if __name__ == '__main__':
                         help='UCB parameter.')
     parser.add_argument('--sigma', type=float, default=1.0,
                         help='Standard deviation of the noise.')
+    parser.add_argument('--te', type=int, default=10,
+                        help='Number of iterations on which to exactly update'
+                             'the inverse matrices.')
     parser.add_argument('--seed', type=int, default=None,
                         help='Set the seed for the experiment. Can be used '
                         'for debug or to freeze experiments.')
@@ -55,79 +58,87 @@ if __name__ == '__main__':
                         help='Verbosity level.')
     args = parser.parse_args()
 
-###############################################################################
-# Setting the simulation
-bandit_env = env.ClusteredGaussianLinearBandit(
-                                N=args.N, T=args.T, d=args.d, K=args.K,
-                                n_thetas=1, sigma=args.sigma,
-                                shuffle_labels=False, seed=args.seed)
+    ###########################################################################
+    # Setting the simulation
+    if args.verbose:
+        print("[main] Setting simulation.")
 
-bandit_controller = controller.DecentralizedController(
-                        N=args.N, agent_cls=agents.LinUCB,
-                        agent_kwargs={'arms': bandit_env.arms,
-                                      'alpha': args.alpha})
+    bandit_env = env.ClusteredGaussianLinearBandit(
+                                    N=args.N, T=args.T, d=args.d, K=args.K,
+                                    n_thetas=1, sigma=args.sigma,
+                                    shuffle_labels=False, seed=args.seed)
 
-rng = utils.check_random_state(args.seed)
-seeds = rng.randint(MAX_RANDINT, size=args.n_trials)
+    bandit_controller = controller.DecentralizedController(
+                            N=args.N,
+                            agent_cls=multi_agents.LinUCB,
+                            agent_kwargs={'alpha': args.alpha,
+                                          'arms': bandit_env.arms,
+                                          'te': args.te,
+                                          'return_arm_index': True,
+                                          'seed': args.seed},
+                                        )
 
-###############################################################################
-# Run the simulation
-trial_results = run_trials(bandit_env, bandit_controller,
-                           controller_stop=False, seeds=seeds,
-                           n_jobs=args.n_jobs, verbose=args.verbose)
+    rng = utils.check_random_state(args.seed)
+    seeds = rng.randint(MAX_RANDINT, size=args.n_trials)
 
-###############################################################################
-# Gathering results
-rewards, best_rewards, regrets = [], [], []
-for trial_result in trial_results:
+    ###########################################################################
+    # Run the simulation
+    if args.verbose:
+        print("[main] Running simulation.")
 
-    _, mean_regret, mean_reward, mean_best_reward, _, _, _, _ = trial_result
+    trial_results = run_trials(bandit_env, bandit_controller,
+                               controller_stop=False, seeds=seeds,
+                               n_jobs=args.n_jobs, verbose=args.verbose)
 
-    regrets.append(mean_regret)
-    rewards.append(mean_reward)
-    best_rewards.append(mean_best_reward)
+    ###########################################################################
+    # Gathering results
+    if args.verbose:
+        print("[main] Gathering results.")
 
-###############################################################################
-# Plotting
+    cumulative_regrets, instantaneous_regret = [], []
+    for trial_result in trial_results:
 
-fig, axis = plt.subplots(nrows=1, ncols=2, figsize=(9, 3), squeeze=False)
+        controller, env = trial_result
 
-# regret
-mean_, std_, _, _, all_lengths = utils.tolerant_stats(regrets)
-tt = np.arange(np.max(all_lengths))
+        instantaneous_regret.append(env.mean_instantaneous_regret())
+        cumulative_regrets.append(np.cumsum(env.mean_instantaneous_regret()))
 
-axis[0, 0].plot(tt, mean_, color='tab:blue', lw=3.0, linestyle='solid', )
-axis[0, 0].fill_between(tt, mean_ + std_, mean_ - std_, color='tab:blue',
-                        alpha=0.2)
+    ###########################################################################
+    # Plotting
+    fig, axis = plt.subplots(nrows=1, ncols=2, figsize=(9, 3), squeeze=False)
 
-axis[0, 0].set_xlabel('t', fontsize=16)
-axis[0, 0].set_ylabel('Cumulative regret')
-axis[0, 0].set_xscale('log')
-axis[0, 0].set_yscale('log')
+    # instantaneous regret
+    mean_, std_, _, _, all_lengths = utils.tolerant_stats(instantaneous_regret)
+    tt = np.arange(np.max(all_lengths))
 
-axis[0, 0].grid()
+    axis[0, 0].plot(tt, mean_, color='tab:blue', lw=3.0, linestyle='solid', )
+    axis[0, 0].fill_between(tt, mean_ + std_, mean_ - std_, color='tab:blue',
+                            alpha=0.2)
 
-# cumulative reward
-mean_rewards, std_rewards, _, _, _ = utils.tolerant_stats(rewards)
-mean_best_rewards, _, _, _, _ = utils.tolerant_stats(best_rewards)
+    axis[0, 0].set_xlabel(r'$t$')
+    axis[0, 0].set_ylabel(r'$r_t$')
+    axis[0, 0].set_ylim(0.0)
+    axis[0, 0].set_xscale('log')
 
-axis[0, 1].plot(tt, mean_rewards, color='tab:blue', lw=3.0, linestyle='solid',
-                label=r'S_t')
-axis[0, 1].fill_between(tt, mean_rewards + std_rewards,
-                        mean_rewards - std_rewards, color='tab:blue',
-                        alpha=0.2)
-axis[0, 1].plot(tt, mean_best_rewards, color='tab:red', lw=3.0,
-                linestyle='dashed',label=r'S_t$^{\text{max}}$')
+    axis[0, 0].grid()
 
-axis[0, 1].legend(loc='center left', bbox_to_anchor=(1.0, 0.6), ncol=1)
+    # cumulative instantaneous regret
+    mean_, std_, _, _, _ = utils.tolerant_stats(cumulative_regrets)
 
-axis[0, 1].set_xlabel('t')
-axis[0, 1].set_ylabel('Cumulative rewards')
-axis[0, 1].set_xscale('log')
-axis[0, 1].set_yscale('log')
+    axis[0, 1].plot(tt, mean_, color='tab:blue', lw=3.0, linestyle='solid')
+    axis[0, 1].fill_between(tt, mean_ + std_, mean_ - std_, color='tab:blue',
+                            alpha=0.2)
 
-axis[0, 1].grid()
+    axis[0, 1].set_xlabel(r'$t$')
+    axis[0, 1].set_ylabel(r'$R_t$')
+    axis[0, 1].set_xscale('log')
+    axis[0, 1].set_yscale('log')
 
-fig.tight_layout()
+    axis[0, 1].grid()
 
-plt.savefig(args.fig_fname, dpi=300)
+    fig.tight_layout()
+
+    if args.verbose:
+        print(f"[main] Saving plot under '{args.fig_fname}'.")
+
+    plt.savefig(args.fig_fname, dpi=300)
